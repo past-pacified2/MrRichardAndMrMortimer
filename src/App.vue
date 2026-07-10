3281c47a8cec5b4785da6b005df951969dc8037f
laszl <laszlo.vt@gmail.com>
2026-07-10 14:37:40 +0200

    chore: saffold vite vue-ts app Removed boilerplate and added basic css for the general high-level layout

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <h1>Rick & Morty</h1>
+  </header>
+
+  <main>
+    <div>Rick & Morty API SPA</div>
+  </main>
+
+  <footer>
+    <div>© {{ new Date().getFullYear() }} Rick & Morty API SPA</div>
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+  footer {
+    margin-top: auto;
+  }
+</style>
